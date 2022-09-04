e3cac39ad81ee3b3e958c88c8dcbdd3a4192b655
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2022-09-04 01:25:32 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_List = []
     for x in matrix:
-	new_List.append(list(map(lambda x: x ** 2, x)))
-    return (new_List)
+	new_List.append([y ** 2 for y in x])
+    return new_List
