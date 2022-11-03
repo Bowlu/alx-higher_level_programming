582ac2ab6e46940a455099ed6b9f032dac3c73e2
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2022-11-03 03:07:08 +0100

    corrected table name

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,6 @@
 -- create a table in the current MYSQL server database
 
-CREATE TABLE IF NOT EXISTS first_name (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (
+  id INT, 
+  name VARCHAR(256),
+);
