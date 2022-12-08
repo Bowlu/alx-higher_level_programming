8ed4a75ac47ae9ff2222444ff686ea72eed44dd0
Awoyemi Boluwatife <awoyemiboluwatife@gmail.com>
2022-12-08 04:57:37 +0000

    citites in states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+script that lists the state name entered as an argument
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    start_db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            )
+
+    cur = start_db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name='{}' 
+            ORDER by id ASC""".format(argv[4]))
+
+    rows_query = cur.fetchall()
+    for row in rows_query:
+        if row[1] == argv[4]:
+            print(row)
+
+    # Closing the cursor
+    cur.close()
+    # Closing the database
+    start_db.close()
