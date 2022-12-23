2cbdb0c895cab457ca21466efa8a91fc5af216f6
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2022-12-23 11:30:35 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
-
-function factorial (number) {
-	if (isNaN(parseInt(number))) {
-		return (1);
-	}
-	if (number === 1) {
-		return (1);
-	}
-	return (number * factorial(number - 1));
+const x = parseInt(process.argv[2]);
+function factorial (x) {
+  return x === 0 || isNaN(x) ? 1 : (x * factorial(x - 1));
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+console.log(factorial(x));
