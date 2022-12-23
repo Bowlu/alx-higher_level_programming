686d8ca02c5df6a0427ca1724322210b3b063f28
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2022-12-23 11:31:22 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,9 @@
 #!/usr/bin/node
-
-function second (myArray) {
-	if (myArray.length === 2 || myArray.length === 3) { return (0); }
-
-	let max = myArray[2];
-	let secondMax = myArray[3];
-
-	for (let i = 2; i < myArray.length; i++) {
-		if (myArray[i] > max) {
-			secondMax = max;
-			max = myArray[i];
-		} else if (myArray[i] > secondMax && myArray[i] < max) {
-			secondMax = myArray[i];
-		}
-	}
-	return (secondMax);
+const myVar = process.argv.length;
+if (myVar <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.slice(2).map(x => parseInt(x));
+  const sorted = args.sort((a, b) => a < b);
+  console.log(sorted[1]);
 }
-
-console.log(second(process.argv));
