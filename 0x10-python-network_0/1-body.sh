3205b9c22e1d429f2722830796c0da186e71396e
Awoyemi Boluwatife <awoyemiboluwatife@gmail.com>
2022-12-23 08:37:00 +0000

    finding a peak

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# script that takes in a URL and displays the body
+
+curl -sL $1
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# script that sends a DELETE request to the URL passed 
+
+curl -s "$1" -X DELETE
