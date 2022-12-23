255af21fa67d885f016e05f92fb4e63bcdd192e5
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2022-12-23 10:37:27 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,15 +3,14 @@
     # Script that fetches https://alx-intranet.hbtn.io/status
     #status
 """
-from urllib.request import urlopen
-from sys import argv
-
-
-def requestId():
-    """opening url and performing task"""
-    with urlopen(argv[1]) as response:
-        print(response.headers.get('X-Request-Id'))
-
+import urllib.request
 
 if __name__ == '__main__':
-    requestId()
+    url = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(url) as res:
+        html = res.read()
+
+        print("Body response:")
+        print("\t - type: {}".format(type(html)))
+        print("\t - content: {}".format(html))
+        print("\t - utf8 content: {}".format(html.decode('utf8')))
